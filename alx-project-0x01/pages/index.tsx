55edf8af80558ff07b1d3fcdceb675e8937ec220
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-26 00:53:53 +0200

    missing space

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -2,7 +2,7 @@ import Header from "../components/layout/Header";
 const Home: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1>Welcome to our Application!</h1>
+      <h1> Welcome to our Application!</h1>
     </div>
   )
 }
