7c4f20e008ab73969f095e0010ef5cdeab47e420
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-26 00:36:34 +0200

    save users

--- alx-project-0x01/components/layout/Header.tsx
+++ alx-project-0x01/components/layout/Header.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-
+import Link from "next/link";
 const Header: React.FC = () => {
     return (
         <header>
             <h1>My Application</h1>
+            <Link href='/user'>Users</Link>
         </header>
     );
 };
