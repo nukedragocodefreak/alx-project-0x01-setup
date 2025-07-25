1a188f495ac7e7821e282da1a79b41b930bbf9d9
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-26 01:10:44 +0200

    fix bugs

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,25 +1,36 @@
-import React from "react";
-import Button from "../../components/common/Button";
 
-const UserPage: React.FC = () =>{
-    return(
-        <div>
-        <h2>First of MNY</h2>
-        <Button title= "My First Post" content = "The future is here..." />
-        </div>
-    );
-};
+import Head from "next/head";
+import { UserProps } from "../../interfaces";
+import UserCard from "../../components/common/UserCard";
 
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users")
-  const posts = await response.json()
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const posts = await response.json();
 
   return {
     props: {
-      posts
-    }
-  }
+      posts,
+    },
+  };
 }
 
+const Users = ({ posts }: { posts: UserProps[] }) => {
+  return (
+    <>
+      <Head>
+        <title>Users</title>
+      </Head>
+      <main className="p-6 bg-gray-50 min-h-screen">
+        <h1 className="text-3xl font-bold mb-6">User Directory</h1>
+        <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+          {posts.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </div>
+      </main>
+    </>
+  );
+};
 
-export default UserPage
+export default Users;
+
