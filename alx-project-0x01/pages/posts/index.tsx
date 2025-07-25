350bba932b1f6cafd28c478547076d22ff792121
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-26 01:08:38 +0200

    current changes

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,6 +1,7 @@
-import PostCard from "@/components/common/PostCard";
-import Header from "@/components/layout/Header";
-import { PostProps } from "@/interfaces";
+import PostCard from "../../components/common/PostCard"
+import Header from "../../components/layout/Header"
+import { PostProps } from "../../interfaces"
+
 
 const Posts: React.FC<PostProps[]> = ({ posts }) => {
   console.log(posts)
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -10,4 +10,16 @@ const UserPage: React.FC = () =>{
     );
 };
 
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+
 export default UserPage
