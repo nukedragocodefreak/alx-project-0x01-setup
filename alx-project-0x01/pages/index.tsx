c35d90e3360995d76d8a1bb7a7ed0a259bf6ca37
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-26 00:52:13 +0200

    fix issue that fails on tests

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -1,10 +1,8 @@
 import Header from "../components/layout/Header";
-const Home: React.FC = () => {
+export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
       <h1 className="text-7xl font-thin">Welcome to our Application!</h1>
     </div>
-  )
+  );
 }
-
- export default Home;
