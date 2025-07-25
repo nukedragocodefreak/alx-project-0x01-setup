82a5bb2cd603ac0d97e7dfb7e24707a23c073c37
Noel Mangwarira <ncmangwarira@gmail.com>
2025-07-26 00:53:12 +0200

    remove h1

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -1,8 +1,10 @@
 import Header from "../components/layout/Header";
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <h1 className="text-7xl font-thin">Welcome to our Application!</h1>
+      <h1>Welcome to our Application!</h1>
     </div>
-  );
+  )
 }
+
+ export default Home;
